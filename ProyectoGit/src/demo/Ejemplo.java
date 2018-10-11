dc201a9360fbc30f0aec7edaf0bffbd1c9c9bfdd
SUITE <SUITE@SUITEE102-ST25.cibertec.edu.pe>
2018-10-10 20:52:17 -0500

    Prueba2

--- ProyectoGit/src/demo/Ejemplo.java
+++ ProyectoGit/src/demo/Ejemplo.java
@@ -6,6 +6,13 @@ public class Ejemplo {
 		System.out.println("Git Gub");
 	}
  public void metodo1(){
-	 
+	 System.out.println("Hola");
  }
+ 
+public void metodo2(){
+	System.out.println("Venecas");
+ }
+public void metodo3(){
+	System.out.println("Pack");
+}
 }
