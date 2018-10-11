6ed89091987e8d932155698dcefa296c2be73d8d
SUITE <SUITE@SUITEE102-ST25.cibertec.edu.pe>
2018-10-10 21:10:10 -0500

    Hola

---
+++ PuntosGit/src/Demo/Prueba.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Prueba {
+	
+	public static void main(String[]args){
+		System.out.println("Git");
+	}
+ public void metodo1(){
+	 System.out.println("Hola Mundo");
+ }
+}
